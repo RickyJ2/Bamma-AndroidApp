5d9484176da933ea11b0f23d05821dc685627e78
EnriqueTrisfan <enriquetrisfan01@gmail.com>
2022-09-11 06:17:32 +0700

    Enrique_BottomNavBar

---
+++ app/src/main/java/com/example/sewakendaraan/Home.kt
@@ -0,0 +1,40 @@
+package com.example.sewakendaraan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.sewakendaraan.databinding.ActivityHomeBinding
+
+class Home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+
+                else -> {
+
+                }
+
+            }
+            true
+        }
+    }
+
+    private fun  replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+    }
+}
