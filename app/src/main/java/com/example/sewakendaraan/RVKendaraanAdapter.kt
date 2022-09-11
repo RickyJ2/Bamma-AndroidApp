c181247c422d6760900462433537b23c20f4f715
EnriqueTrisfan <enriquetrisfan01@gmail.com>
2022-09-11 10:44:07 +0700

    Enrique_BottomNavBar

---
+++ app/src/main/java/com/example/sewakendaraan/RVKendaraanAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.sewakendaraan
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sewakendaraan.entity.Kendaraan
+
+class RVKendaraanAdapter(private val data: Array<Kendaraan>): RecyclerView.Adapter<RVKendaraanAdapter.viewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder{
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.rv_item_kendaraan, parent, false)
+        return viewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        val currentItem = data[position]
+        holder.tvNamaPemilik.text = currentItem.namaPemilik
+        holder.tvDetails.text = currentItem.kendaraan
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class viewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val tvNamaPemilik: TextView = itemView.findViewById(R.id.tvNamaPemilik)
+        val tvDetails: TextView = itemView.findViewById(R.id.tvDetails)
+    }
+}
---
+++ app/src/main/java/com/example/sewakendaraan/entity/entity.kt
@@ -0,0 +1,18 @@
+package com.example.sewakendaraan.entity
+
+class Kendaraan (var namaPemilik: String, var kendaraan: String){
+    companion object{
+        var listOfKendaraan = arrayOf(
+            Kendaraan("Enrique", "Toyota Supra"),
+            Kendaraan("Bonifasius", "Porsche 718 Cayman"),
+            Kendaraan("Ricky", "Lamborghini Aventador"),
+            Kendaraan("Alice", "Sweeptail Rolls Royce"),
+            Kendaraan("Jonathan", "Ferrari California"),
+            Kendaraan("Kevin", "Toyota Supra"),
+            Kendaraan("Hendra", "Porsche 718 Cayman"),
+            Kendaraan("Ridho", "Toyota Supra"),
+            Kendaraan("Santi", "Mercedes Benz Maybach"),
+            Kendaraan("Joshua", "Koenigsegg"),
+        )
+    }
+}
